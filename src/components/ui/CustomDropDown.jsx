ba8f65fb76c936f6356ac8a9ca2b174fe85375f3
Aman <b121061@iiit-bh.ac.in>
2025-01-21 16:15:44 +0530

    [Feat] :- Keyboard Navigation in custom Dropdown

--- src/components/ui/CustomDropDown.jsx
+++ src/components/ui/CustomDropDown.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState, useMemo, useCallback } from "react";
+
 import { Button } from "@/components/ui/button";
+
 import {
   Command,
   CommandEmpty,
@@ -8,6 +10,7 @@ import {
   CommandItem,
   CommandList
 } from "@/components/ui/command";
+
 import {
   Popover,
   PopoverContent,
@@ -41,11 +44,11 @@ const CustomDropDown = ({
   vendor_id,
   branch_id
 }) => {
-
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(Value || "");
   const [item, setItem] = useState(null);
   const [itemsArray, setItemsArray] = useState([]);
+  const [focusedIndex, setFocusedIndex] = useState(-1);
 
   useEffect(() => {
     if (Value !== undefined) {
@@ -73,7 +76,17 @@ const CustomDropDown = ({
     },
     [value, multiSelect, onChange, itemsArray, data]
   );
-
+  useEffect(() => {
+    if (focusedIndex >= 0) {
+      const focusedItem = document.getElementById(`dropdown-item-${focusedIndex}`);
+      if (focusedItem) {
+        focusedItem.scrollIntoView({
+          behavior: "smooth",
+          block: "nearest",
+        });
+      }
+    }
+  }, [focusedIndex]);
   const sortedData = useMemo(() => {
     return [...data].sort((a, b) => {
       if (itemsArray.includes(a.value)) return -1;
@@ -144,6 +157,30 @@ const CustomDropDown = ({
     );
   };
 
+  const handleKeyDown = (event) => {
+    // event.stopPropagation()
+    if (!open) return;
+    console.log(event.key)
+
+    if (event.key === "ArrowDown") {
+      event.preventDefault();
+      setFocusedIndex((prevIndex) =>
+        prevIndex < sortedData.length - 1 ? Number(prevIndex) + 1 : Number(prevIndex)
+      );
+    } else if (event.key === "ArrowUp") {
+      event.preventDefault();
+      setFocusedIndex((prevIndex) =>
+        prevIndex > 0 ? Number(prevIndex) - 1 : 0
+      );
+    } else if (event.key === "Enter") {
+      event.preventDefault();
+      if (focusedIndex >= 0 && focusedIndex < sortedData.length) {
+       
+        handleSelect(sortedData[focusedIndex].value, sortedData[focusedIndex]);
+      }
+    }
+  };
+
   return (
     <Popover
       open={open}
@@ -162,6 +199,7 @@ const CustomDropDown = ({
             "min-w-fit border h-[2.5rem] dark:bg-[#000000] dark:text-textColor/200 dark:border-[#000000] bg-[#FFFFFF] hover:bg-[#FFFFFF] border-[#E0E0E0] justify-between capitalize shadow-none !rounded-[4px] text-[#000000] hover:text-[#666666] font-poppins font-normal text-xs !z-10",
             multiSelect && itemsArray?.length > 0 && "!bg-primary !text-white"
           )}
+          onKeyDown={handleKeyDown}
         >
           {renderTriggerContent()}
           <ChevronDown
@@ -174,6 +212,7 @@ const CustomDropDown = ({
         </Button>
       </PopoverTrigger>
       <PopoverContent
+      onKeyDown={handleKeyDown}
         className={`${className} p-0 dark:border-[#051C14] w-fit !max-w-80 mr-1 !z-50 `}
         contentClassName={`${contentClassName} w-full`}
       >
@@ -187,40 +226,40 @@ const CustomDropDown = ({
             <CommandGroup className=" !z-50 !ml-0 ">
               {showCustomItems
                 ? children
-                : sortedData.map((item) => (
-                    <CommandItem
-                      key={item.value}
-                      className={cn(
-                        "text-left border flex items-start justify-normal  mb-1.5 !pl-0 border-[#E0E0E0] !bg-gray-200/70 !z-50  ",
-                        multiSelect && "!pl-2"
-                      )}
-                      onBlur={onBlur}
-                      onSelect={() => {
-                        !multiSelect && handleSelect(item.value, item);
-                      }}
-                    >
-                     
+                : sortedData.map((item, index) => (
+                  <CommandItem
+                  key={item.value}
+                  id={`dropdown-item-${index}`}
+                  className={cn(
+                    "text-left flex items-start justify-normal mb-1.5 !pl-0 border-[#E0E0E0] !bg-gray-200/70 !z-50",
+                    multiSelect && "!pl-2",
+                    focusedIndex === index && "!border-black !border !bg-gray-300/40"
+                  )}
+                  onBlur={onBlur}
+                  onSelect={() => {
+                    if (!multiSelect) handleSelect(item.value, item);
+                  }}
+                >
                       <div className="flex justify-between w-full items-center !min-w-56 !pl-0 font-poppins text-xs font-normal dark:!text-[#FFFFFF] gap-x-4   !ml-0">
                         <div className="flex items-center gap-x-2 ">
-                      
                           {item?.archived_status ? (
                             <Archive className="h-4 w-4 text-yellow-500" />
                           ) : (
                             <div className="w-5 h-5" />
                           )}
                           <span className="capitalize text-left  flex items-center gap-x-2">
-                          {!multiSelect && (
-                        <Check
-                          className={cn(
-                            " h-4 w-4 dark:text-[#FFFFFF] ",
-                            value === item.value ? "opacity-100" : "opacity-0"
-                          )}
-                        />
-                      )}
-                            {item?.label || item?.value}
+                            {!multiSelect && (
+                              <Check
+                                className={cn(
+                                  " h-4 w-4 dark:text-[#FFFFFF] ",
+                                  value === item.value ? "opacity-100" : "opacity-0"
+                                )}
+                              />
+                            )}
+                            {item?.label || item?.value} 
                           </span>
                         </div>
-                        <div className="flex gap-x-2 items-center">
+                        <div className="flex gap-x-2 items-center pr-2">
                           {item?.human_verified && (
                             <img
                               src={approved}
@@ -230,6 +269,7 @@ const CustomDropDown = ({
                           )}
                           {multiSelect && (
                             <Checkbox
+                            className=""
                               checked={itemsArray.includes(item.value)}
                               onCheckedChange={(checked) => {
                                 setItemsArray((prev) => {
