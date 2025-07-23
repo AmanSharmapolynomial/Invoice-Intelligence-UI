ad3bd37c851afaf5c449b01ef14e1f3887488b8c
AmanSharmapolynomial <161309757+AmanSharmapolynomial@users.noreply.github.com>
2025-07-23 17:03:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import ErrorBoundary from "./components/common/ErrorBoundaries.jsx";
 // Initialize Sentry before rendering
 Sentry.init({
   dsn: "https://bc3120adb6c43cadd8982712658c11a6@o4509682681905152.ingest.us.sentry.io/4509688521228288",
-  sendDefaultPii: true,
+  sendDefaultPii: false,
   integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1.0,
   tracePropagationTargets: [/^https:\/\/invoice-intelligence-ui-prod\.vercel\.app/],
